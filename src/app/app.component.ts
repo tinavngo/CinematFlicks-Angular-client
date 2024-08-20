008044e07ebbd61a6bf62bc9b9cf36fc69e704a6
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-08-19 19:38:08 -0700

    Remove unnecessary components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
